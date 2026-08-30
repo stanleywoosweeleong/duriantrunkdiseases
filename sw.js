3bc9c67d987d25ef2d973b3ce2037ef5ccf330a6
stanleywoosweeleong <130345527+stanleywoosweeleong@users.noreply.github.com>
2026-08-30 20:21:22 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
  *
  * Bump CACHE_VERSION on every deploy. That is the whole update mechanism.
  */
-const CACHE_VERSION = 'dtd-v2026-08-30-4';
+const CACHE_VERSION = 'dtd-v2026-08-30-5';
 const CACHE = CACHE_VERSION;
 
 const PRECACHE = [
